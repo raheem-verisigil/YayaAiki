09ef4cbd164bbfffd0d1c557529d5b286d52d056
raheem-verisigil <2.80984891e+08+raheem-verisigil@users.noreply.github.com>
2026-09-18 22:20:13 +0000

    Add OpenAPI and Swagger API docs

--- server/_core/index.ts
+++ server/_core/index.ts
@@ -4,6 +4,7 @@ import { createServer } from "http";
 import net from "net";
 import { createExpressMiddleware } from "@trpc/server/adapters/express";
 import { registerStorageProxy } from "./storageProxy";
+import { registerOpenApiRoutes } from "./openapi";
 import { appRouter } from "../routers";
 import { createContext } from "./context";
 import { serveStatic, setupVite } from "./vite";
@@ -29,6 +30,7 @@ async function startServer() {
   app.use(express.json({ limit: "50mb" }));
   app.use(express.urlencoded({ limit: "50mb", extended: true }));
   registerStorageProxy(app);
+  registerOpenApiRoutes(app);
   app.use("/api/trpc", createExpressMiddleware({ router: appRouter, createContext }));
 
   if (process.env.NODE_ENV === "development") await setupVite(app, server);
