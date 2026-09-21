82604be32139ee5f66a037f0ec4868ac7b6942b2
Your Name <raheem@verisigilai.com>
2026-09-21 22:56:18 +0100

    Wire intakeRouter into appRouter

--- server/routers.ts
+++ server/routers.ts
@@ -2,11 +2,13 @@ import { COOKIE_NAME } from "@shared/const";
 import { getSessionCookieOptions } from "./_core/cookies";
 import { systemRouter } from "./_core/systemRouter";
 import { workEngineRouter } from "./_core/workEngineRouter";
+import { intakeRouter } from "./_core/intakeRouter";
 import { publicProcedure, router } from "./_core/trpc";
 
 export const appRouter = router({
   system: systemRouter,
   workEngine: workEngineRouter,
+  intake: intakeRouter,
   auth: router({
     me: publicProcedure.query(opts => opts.ctx.user),
     logout: publicProcedure.mutation(({ ctx }) => {
